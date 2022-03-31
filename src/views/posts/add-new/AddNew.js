77f8544651c6931364fb8f6530b521cf60fd1171
deny anggriawan <v-deny.anggriawan@anteraja.id>
2022-04-01 05:31:04 +0700

    discard pretier lint

--- src/views/posts/add-new/AddNew.js
+++ src/views/posts/add-new/AddNew.js
@@ -1,31 +1,44 @@
 import React from 'react'
-import { CButton, CFormCheck, CFormInput, CFormText, CFormLabel, CRow, CForm } from '@coreui/react'
+import {
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+} from '@coreui/react'
 
 const AddNew = () => {
   return (
-    <CRow>
-      <CForm>
-        <div className="mb-3">
-          <CFormLabel htmlFor="exampleInputEmail1">Email address</CFormLabel>
-          <CFormInput type="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <CFormText id="emailHelp">We ll never share your email with anyone else.</CFormText>
-        </div>
-        <div className="mb-3">
-          <CFormLabel htmlFor="exampleInputPassword1">Email Password</CFormLabel>
-          <CFormInput type="password" id="exampleInputPassword1" />
-        </div>
-        <CFormCheck
-          className="mb-3"
-          label="Check me out"
-          onChange={(e) => {
-            console.log(e.target)
-          }}
-        />
-        <CButton type="submit" color="primary">
-          Submit
-        </CButton>
-      </CForm>
-    </CRow>
+    <CTable>
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell scope="col">#</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Class</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
+      <CTableBody>
+        <CTableRow>
+          <CTableHeaderCell scope="row">1</CTableHeaderCell>
+          <CTableDataCell>Mark</CTableDataCell>
+          <CTableDataCell>Otto</CTableDataCell>
+          <CTableDataCell>@mdo</CTableDataCell>
+        </CTableRow>
+        <CTableRow>
+          <CTableHeaderCell scope="row">2</CTableHeaderCell>
+          <CTableDataCell>Jacob</CTableDataCell>
+          <CTableDataCell>Thornton</CTableDataCell>
+          <CTableDataCell>@fat</CTableDataCell>
+        </CTableRow>
+        <CTableRow>
+          <CTableHeaderCell scope="row">3</CTableHeaderCell>
+          <CTableDataCell colSpan="2">Larry the Bird</CTableDataCell>
+          <CTableDataCell>@twitter</CTableDataCell>
+        </CTableRow>
+      </CTableBody>
+    </CTable>
   )
 }
 
